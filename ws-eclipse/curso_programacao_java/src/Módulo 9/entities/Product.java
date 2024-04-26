c38fcfccb8aebbd5d30815b9af572ca8d417e7ad
Davi <oversouls11@gmail.com>
2024-04-26 20:12:57 -0300

    Continuação estudos

---
+++ ws-eclipse/curso_programacao_java/src/Módulo 9/entities/Product.java
@@ -0,0 +1,68 @@
+package entities;
+
+public class Product {
+	
+	private String name;
+	private double price;
+	private int quantity;
+	
+	// Contrutor padrão
+	public Product() {
+	}
+	
+	// Construtor tem nome da classe e inicia no começo do objeto
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	// Construtor de sobrecarga ele tem caracteristicas parecidas, porém faz coisas diferentes
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public double totalValueInStock() {
+		return this.price * this.quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return 	"Product data: "
+				+ this.name  
+				+ ", $ " 
+				+ String.format("%.2f", this.price)
+				+ ", "
+				+ this.quantity
+				+ " units, Total: $ "
+				+ String.format("%.2f", this.totalValueInStock());
+	}
+}
