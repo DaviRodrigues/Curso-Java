b721012a85f3883752dc563bdba24719c040bfa1
DaviRodrigues <oversouls11@gmail.com>
2025-02-26 13:56:43 -0300

    Continuação Estudos

---
+++ Seções/Seção-16/Aula-1/aula1/src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static final Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
