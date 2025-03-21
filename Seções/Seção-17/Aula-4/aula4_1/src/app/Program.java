5aef83ff680567a5820a637795a73d3fd6a16485
DaviRodrigues <oversouls11@gmail.com>
2025-03-21 13:57:14 -0300

    Continuação Estudos

--- Seções/Seção-17/Aula-4/aula4_1/src/app/Program.java
+++ Seções/Seção-17/Aula-4/aula4_1/src/app/Program.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
@@ -17,17 +18,40 @@ public class Program {
 		
 		//list.add(20);
 		
+//		List<Object> myObjs = new ArrayList<Object>();
+//		myObjs.add("Maria");
+//		myObjs.add("Alex");
+//		
+//		List<? super Number> myNums = myObjs;
+//		
+//		myNums.add(10);
+//		myNums.add(3.14);
+//		
+//		Number x = myNums.get(0);
+		
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
 		
-		List<? super Number> myNums = myObjs;
+		copy(myInts, myObjs);
+		printList(myObjs);
 		
-		myNums.add(10);
-		myNums.add(3.14);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 		
-		Number x = myNums.get(0);
-		
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destination) {
+		for (Number number: source) {
+			destination.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 
 }
---
+++ Seções/Seção-17/Aula-5/aula5/src/app/Program.java
@@ -0,0 +1,23 @@
+package app;
+
+import entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String t1 = "Test";
+		String t2 = "Test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c2);
+		System.out.println(t1 == t2);
+		
+	}
+
+}
